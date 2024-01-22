13f3c9909057c8d37b72cd3de07711b015fa19bf
Zeyad Ahmed <zeyadahmed10101@gmail.com>
2024-01-22 15:09:35 +0200

    Adding Resttemplate bean in main class

--- src/main/java/com/zeyad/securefileacess/SecurefileacessApplication.java
+++ src/main/java/com/zeyad/securefileacess/SecurefileacessApplication.java
@@ -2,6 +2,8 @@ package com.zeyad.securefileacess;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SecurefileacessApplication {
@@ -9,5 +11,8 @@ public class SecurefileacessApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SecurefileacessApplication.class, args);
 	}
-
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 }
