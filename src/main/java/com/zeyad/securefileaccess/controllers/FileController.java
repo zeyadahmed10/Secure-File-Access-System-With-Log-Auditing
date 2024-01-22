05427098fb28bf3dcdc7da421b4fecf8f94a34df
Zeyad Ahmed <zeyadahmed10101@gmail.com>
2024-01-22 18:18:11 +0200

    Adding method signatures for FileService and call the methods in FileController

--- src/main/java/com/zeyad/securefileaccess/controllers/FileController.java
+++ src/main/java/com/zeyad/securefileaccess/controllers/FileController.java
@@ -8,20 +8,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RequestMapping("/api/v1/files")
 public class FileController {
     @Autowired
     private FileService fileService;
 
     @GetMapping
-    public FileResponseDTO getFiles(@RequestParam(name = "name", defaultValue = "") String name,
-                                    @RequestParam(name = "page", defaultValue = "0") Integer page,
-                                    @RequestParam(name = "size", defaultValue = "10") Integer size){
+    public List<FileResponseDTO> getFiles(@RequestParam(name = "name", defaultValue = "") String name,
+                                          @RequestParam(name = "page", defaultValue = "0") Integer page,
+                                          @RequestParam(name = "size", defaultValue = "10") Integer size){
         return fileService.findAll();
     }
     @GetMapping("/{id}")
     public FileResponseDTO getFileById(@PathVariable Integer id){
-        return fileService.findAll();
+        return fileService.findById();
     }
     @PostMapping
     public FileResponseDTO addFile(@RequestBody FileRequestDTO fileRequestDTO){
@@ -29,14 +31,17 @@ public class FileController {
     }
     @PostMapping("/{id}/grant-access")
     public ResponseEntity<String> grantAccess(@PathVariable Integer id, @RequestBody GrantAccessRequestDTO grantAccessRequestDTO){
+        fileService.grantAccess(id, grantAccessRequestDTO);
         return ResponseEntity.ok("Access granted successfully");
     }
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteFile(@PathVariable Integer id){
+        fileService.deleteFile(id);
         return ResponseEntity.noContent().build();
     }
     @PatchMapping("/{id}")
     public ResponseEntity<Void> updateFile(@PathVariable Integer id, @RequestBody FileRequestDTO fileRequestDTO){
+        fileService.updateFile(id, fileRequestDTO);
         return ResponseEntity.noContent().build();
     }
 
--- src/main/java/com/zeyad/securefileaccess/services/FileService.java
+++ src/main/java/com/zeyad/securefileaccess/services/FileService.java
@@ -1,12 +1,29 @@
 package com.zeyad.securefileaccess.services;
 
+import com.zeyad.securefileaccess.dto.request.FileRequestDTO;
+import com.zeyad.securefileaccess.dto.request.GrantAccessRequestDTO;
 import com.zeyad.securefileaccess.dto.response.FileResponseDTO;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class FileService {
 
-    public FileResponseDTO findAll(){
-        return new FileResponseDTO();
+    public List<FileResponseDTO> findAll(){
+        return null;
+    }
+
+    public FileResponseDTO findById() {
+        return null;
+    }
+
+    public void grantAccess(Integer id, GrantAccessRequestDTO grantAccessRequestDTO) {
+    }
+
+    public void deleteFile(Integer id) {
+    }
+
+    public void updateFile(Integer id, FileRequestDTO fileRequestDTO) {
     }
 }
